da77ca253f803f3256d938e3e0b2d6e672a9393a
Tyler Payne <tyler43636@gmail.com>
2012-02-01 06:56:16 -0500

    script works now

--- net-monitor.sh
+++ net-monitor.sh
@@ -3,6 +3,14 @@
 #Shows active connections of a user defined process
 #
 
+#Check for root user
+if [ $UID != 0 ]; then
+	echo "Please run as root"
+	exit 0
+fi
+
+#Get process from command line user. 
+#@Add option to enter a process if not passed at runtime
 PROCESS=$1
-echo $PROCESS
+
 lsof -i -n -P | grep $PROCESS
