9f412f607868a70604190a753b79c8b6c664d7de
Tyler Payne <tyler43636@gmail.com>
2012-02-02 02:34:17 -0500

    Changed name to net-connections.sh

---
+++ net-connections.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#net-connect.sh
+#Shows active connections of a user defined process
+#
+
+#Check for root user
+if [ $UID != 0 ]; then
+	echo "Please run as root"
+	exit 0
+fi
+
+#Get process from command line user. 
+#@Add option to enter a process if not passed at runtime
+PROCESS=$1
+
+if [ -z "$PROCESS" ];then
+	echo "Please enter a process to search for:"
+	read PROCESS
+fi
+
+#Test reading user variable
+echo $PROCESS
+
+lsof -i -n -P | grep $PROCESS
